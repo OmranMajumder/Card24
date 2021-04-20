d9cf0a52b10c555baa3b6165c847032a03b4bf65
OmranMajumder <omran.majumder@gmail.com>
2021-04-20 12:42:46 -0400

    Wrote methods to remove spaces and validate parentheses; incomplete code for evaluating expressions

--- Card24/src/card24/FXMLDocumentController.java
+++ Card24/src/card24/FXMLDocumentController.java
@@ -6,6 +6,7 @@
 package card24;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -20,7 +21,10 @@ import javafx.scene.shape.Rectangle;
  * @author omran
  */
 public class FXMLDocumentController implements Initializable {
-
+    String test = " (4 + 2)* 3 + 2";
+    
+    int userNum1, userNum2, userNum3, userNum4;
+    
     @FXML
     private Rectangle cardBox1;
     @FXML
@@ -32,22 +36,114 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label userPrompt;
     @FXML
-    private Button solutionButton;
-    @FXML
     private Button refreshButton;
-    @FXML
-    private Button verifyButton;
     @FXML
     private TextField solutionText;
     @FXML
     private TextField userText;
+    @FXML
+    private Button solveButton;
+    @FXML
+    private Button checkButton;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        test = removeSpaces(test);
+        
+        System.out.println(test);
+        System.out.println(evaluateExpression(test));
     }    
+    
+    public String removeSpaces(String input) {
+        
+        return input.replaceAll(" ", "");
+        
+    }
+    
+    public Boolean validateParentheses(String input) {
+        
+        int rightCount = 0, leftCount = 0;
+        
+        for (int i = 0; i < input.length() - 1; i++) {
+           
+            if (input.charAt(i) == '(') 
+                leftCount++;
+            
+            else if (input.charAt(i) == ')')
+                rightCount++;
+            
+        }
+        
+        if (rightCount == leftCount)           
+            return true;
+        
+        else 
+            return false;
+            
+    }
+    
+    public String evaluateExpression(String input) {
+        
+        int i, j;
+        
+        while (input.contains("(") || input.contains(")")) {
+            // find outside paren, then matching right paren
+            // index towards the middle and start solving outwards
+            for (i = input.length() - 1; i >= 0; i--) {
+                if (input.charAt(i) == '(')
+                    break;
+            }
+            
+            for (j = 0; j < input.length() - 1; j++) {
+                if (input.charAt(j) == ')')
+                    break;
+            }
+            
+            Double tempDouble;
+            String tempString;
+            tempString = input.substring(i + 1, j);
+            
+            
+            while (tempString.contains("*") || tempString.contains("/") 
+                    || tempString.contains("+") || tempString.contains("-")) {
+                
+                ArrayList<String> operands = new ArrayList<String>();
+                ArrayList<String> operators = new ArrayList<String>();
+                
+                for (int k = 0; k < tempString.length() - 1; k++) {
+                    
+                    if (k + 1 < tempString.length() - 1 
+                            && Character.isDigit(tempString.charAt(k)) 
+                            && Character.isDigit(tempString.charAt(k + 1))) {
+                        
+                        operands.add(Character.toString(tempString.charAt(k))
+                                + tempString.charAt(k + 1));
+                        
+                        k++;
+                        
+                    }
+                    
+                    else if (Character.isDigit(tempString.charAt(k))) {
+                        operands.add(Character.toString(tempString.charAt(k)));
+                    }
+                    
+                    else {
+                        operators.add(Character.toString(tempString.charAt(k)));
+                    }
+                    
+                }
+                
+            }
+            
+            input = (input.substring(0, i)).concat(tempString).concat(input.substring(j + 1));
+            
+        }
+        
+        return input;
+        
+    }
     
 }
