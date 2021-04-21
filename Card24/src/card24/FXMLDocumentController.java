90cdcf15dd216352530167080fa3f86f6c59db76
OmranMajumder <omran.majumder@gmail.com>
2021-04-21 09:25:39 -0400

    Completed method for evaluating expressions; needs exception handling for double division; added UML diagram draft to project folder

--- Card24/src/card24/FXMLDocumentController.java
+++ Card24/src/card24/FXMLDocumentController.java
@@ -85,65 +85,140 @@ public class FXMLDocumentController implements Initializable {
             
     }
     
-    public String evaluateExpression(String input) {
+    public int findLastLeftParen(String input) {
         
-        int i, j;
+        int i;
         
-        while (input.contains("(") || input.contains(")")) {
-            // find outside paren, then matching right paren
-            // index towards the middle and start solving outwards
-            for (i = input.length() - 1; i >= 0; i--) {
+        for (i = input.length() - 1; i >= 0; i--) {
                 if (input.charAt(i) == '(')
                     break;
             }
-            
-            for (j = 0; j < input.length() - 1; j++) {
-                if (input.charAt(j) == ')')
-                    break;
-            }
-            
-            Double tempDouble;
-            String tempString;
-            tempString = input.substring(i + 1, j);
-            
-            
-            while (tempString.contains("*") || tempString.contains("/") 
-                    || tempString.contains("+") || tempString.contains("-")) {
-                
-                ArrayList<String> operands = new ArrayList<String>();
-                ArrayList<String> operators = new ArrayList<String>();
-                
-                for (int k = 0; k < tempString.length() - 1; k++) {
-                    
-                    if (k + 1 < tempString.length() - 1 
-                            && Character.isDigit(tempString.charAt(k)) 
-                            && Character.isDigit(tempString.charAt(k + 1))) {
-                        
-                        operands.add(Character.toString(tempString.charAt(k))
-                                + tempString.charAt(k + 1));
-                        
-                        k++;
-                        
-                    }
-                    
-                    else if (Character.isDigit(tempString.charAt(k))) {
-                        operands.add(Character.toString(tempString.charAt(k)));
-                    }
-                    
-                    else {
-                        operators.add(Character.toString(tempString.charAt(k)));
-                    }
-                    
-                }
-                
-            }
-            
-            input = (input.substring(0, i)).concat(tempString).concat(input.substring(j + 1));
-            
+        
+        return i;
+        
+    }
+    
+    public int findFirstRightParen(String input) {
+
+        int i;
+
+        for (i = input.length() - 1; i >= 0; i--) {
+            if (input.charAt(i) == ')')
+                break;
         }
+
+        return i;
+
+    }
+    
+    public String evaluateParen(String input) {
+        
+        int i, j;
+        String tempString;
+            
+        i = findLastLeftParen(input);
+        j = findFirstRightParen(input);
+
+        tempString = input.substring(i + 1, j);
+
+        tempString = evaluateExpression(tempString);
+
+        input = (input.substring(0, i)).concat(tempString).concat(input.substring(j + 1));
         
         return input;
         
     }
+
+    public String evaluateExpression(String input) {
+        
+        if (input.contains("(")) {
+            
+            input = evaluateParen(input);
+            input = evaluateExpression(input);
+            
+        }
+        
+        else {
+            
+            ArrayList<String> operands = new ArrayList<String>();
+            ArrayList<String> operators = new ArrayList<String>();
+        
+            while (input.contains("*") || input.contains("/") 
+                    || input.contains("+") || input.contains("-")) {
+
+                sortOperatorsOperands(input, operators, operands);
+                input = solveArithmetic(operators, operands);
+
+            }
+            
+        }
+        
+        return input;
+         
+    }
+    
+    public String solveArithmetic(ArrayList<String> operators, ArrayList<String> operands) {
+        
+        Integer result = 0;
+            
+        for (int i = 0; i < operators.size(); i++) {
+
+            if (operators.get(i).equals("*") || operators.get(i).equals("/")) {
+
+                if (operators.get(i).equals("*"))
+                    result = Integer.parseInt(operands.get(i)) * Integer.parseInt(operands.get(i + 1));
+
+                else
+                    result = Integer.parseInt(operands.get(i)) / Integer.parseInt(operands.get(i + 1));
+                
+                operands.set(i + 1, result.toString());
+
+            }
+
+            if (operators.get(i).equals("+") || operators.get(i).equals("-")) {
+
+                if (operators.get(i).equals("+"))
+                    result = Integer.parseInt(operands.get(i)) + Integer.parseInt(operands.get(i + 1));
+
+                else
+                    result = Integer.parseInt(operands.get(i)) - Integer.parseInt(operands.get(i + 1));
+                
+                operands.set(i + 1, result.toString());
+
+            }
+
+        }
+        
+        return result.toString();
+        
+    }
+    
+    public void sortOperatorsOperands(String input, ArrayList<String> operators, ArrayList<String> operands) {
+        
+        for (int i = 0; i < input.length(); i++) {
+
+            if (i + 1 < input.length() 
+                    && Character.isDigit(input.charAt(i)) 
+                    && Character.isDigit(input.charAt(i + 1))) {
+
+                operands.add(Character.toString(input.charAt(i))
+                        + input.charAt(i + 1));
+
+                i++;
+
+            }
+
+            else if (Character.isDigit(input.charAt(i))) {
+                operands.add(Character.toString(input.charAt(i)));
+            }
+
+            else {
+                operators.add(Character.toString(input.charAt(i)));
+            }
+
+        }
+        
+    }
+    
     
 }
